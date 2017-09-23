694d90c8bc9ef1c2b4c2df962bfa015731f31201
Sergey <sergeishmaro@gmail.com>
2017-09-23 12:13:50 +0300

    task completed (function check implemented)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
+const isCloseBracket = (config, bracket) => {
+  for (let i = 0; i < config.length; i++) {
+    if (config[i][1] === bracket) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+const isOpenBracket = (config, bracket) => {
+  for (let i = 0; i < config.length; i++) {
+    if (config[i][0] === bracket) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const result = [];
+  for (let i = 0; i < str.length; i++) {
+    let tempOpen = isOpenBracket(bracketsConfig, str[i]);
+    let tempClose = isCloseBracket(bracketsConfig, str[i]);
+
+    // open bracket
+    if (tempOpen !== -1 && tempOpen !== tempClose) {
+      result.push(tempOpen);
+    }
+
+    // close bracket or break loop
+    if (tempClose !== -1 && tempOpen !== tempClose) {
+      if (result[result.length - 1] === tempClose) {
+        result.pop();
+      } else {
+        result.push(false);
+        break;
+      }
+    }
+
+    // check same open and close brackets
+    if (tempOpen === tempClose) {
+      if (result[result.length - 1] === tempClose) {
+        result.pop();
+      } else {
+        result.push(tempOpen);
+      }
+    }
+  }
+  if (result.length) {
+    return false;
+  }
+  return true;
 }
